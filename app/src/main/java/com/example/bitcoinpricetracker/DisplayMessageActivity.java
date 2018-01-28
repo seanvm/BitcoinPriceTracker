baef3ef7735497babdea3c5132dae359c329b5e6
Sean van Mulligen <sean@vanmulligen.ca>
2018-01-28 14:24:53 -0800

    Initial commit

---
+++ app/src/main/java/com/example/bitcoinpricetracker/DisplayMessageActivity.java
@@ -0,0 +1,23 @@
+package com.example.bitcoinpricetracker;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        // Get the intent that triggered this activity and get the extra attributes
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Get the current activity's text box and set it to the message above
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
