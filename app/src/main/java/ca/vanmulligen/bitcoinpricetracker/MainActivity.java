039858dcbfa642421d08564460a63f5fcda762ba
Sean van Mulligen <sean@vanmulligen.ca>
2019-09-05 21:13:40 -0700

    update gradle version and add material UI

--- app/src/main/java/ca/vanmulligen/bitcoinpricetracker/MainActivity.java
+++ app/src/main/java/ca/vanmulligen/bitcoinpricetracker/MainActivity.java
@@ -1,6 +1,6 @@
 package ca.vanmulligen.bitcoinpricetracker;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import ca.vanmulligen.bitcoinpricetracker.exchanges.CoinDesk;
